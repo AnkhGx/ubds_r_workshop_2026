c083f6585849d0e2208b8c288e58dbbe5de0005e
Ankh Creel <ankhcreel@Ankhs-MacBook-Pro.local>
2026-01-29 15:24:56 -0600

    adding first R script

---
+++ analysis/my_r_script.R
@@ -0,0 +1,33 @@
+# this is a comment
+100 + 101
+3 + 5 * 10
+# help me remember order of operations
+3 + (5 * 10)
+# I want to add first
+(3 + 5) * 10
+# do I need a multiplacation symbol?
+(3 + 5) 10
+# yes, I need a multiplacation symbol
+(3 + 5) * 10
+
+# exponents
+3 ^ 2
+
+# are these equal
+10 == 10
+10 == 11
+
+# computers aren't great with decimals
+10.01 == 10.01
+10.01 == 10.00000000001
+10.00 == 10.0000000000000000000000000000000000000000000000000000000001
+
+# let's compare 2 numbers
+2 > 4
+
+# do I need space between operators and number/symbol
+2>4
+
+# what do fractions look like
+2/10000
+500000000000/2
